91d87d2b0d9c1611c9ca2dd839de254e8326a6d2
Winux <157874030+ItsWinuxYT@users.noreply.github.com>
2024-02-23 21:24:26 +0700

    Add files via upload

---
+++ modules/UninstallWorker.bat
@@ -0,0 +1,36 @@
+echo off
+cls
+timeout /t 1 /nobreak > nul
+reg delete HKLM\SOFTWARE\WinuxTools /f > nul
+rd %homedrive%\PROGRA~1\WinuxTools /s /q > nul
+
+rem End screen
+mode 55,25
+cls
+echo.
+echo.
+echo %col%[35m     \\\\\            /////   [[[[[[[[[[]]]]]]]]]]%col%[37m
+echo %col%[35m      \\\\\          /////    [[[[[[[[[[]]]]]]]]]]%col%[37m
+echo %col%[35m       \\\\\        /////            [[[]]]%col%[37m
+echo %col%[35m        \\\\\  /\  /////             [[[]]]%col%[37m
+echo %col%[35m         \\\\\//\\/////              [[[]]]%col%[37m
+echo %col%[35m          \\\\\\//////               [[[]]]%col%[37m
+echo %col%[35m           \\\\\/////                [[[]]]%col%[37m
+echo.
+echo.              An AIO toolbox for your PC.
+echo.                https://linktr.ee/winux
+echo.
+echo.   Thank you for taking your time to try WinuxTools!
+echo.
+echo.           You can always get this software
+echo.           back from Winux's GitHub profile
+echo.               if you change your mind
+echo.
+echo.           Is there anything we can improve?
+echo.       %col%[32m[Y] Yes, open the discord support server!
+echo.              %col%[1;31m[N] No, close the installer
+echo.
+choice /c YN /n /m "%n%                         %col%[0;37m> "
+set input=%errorlevel%
+if "%input%"=="1" start https://discord.gg/GGnfFEyhtQ
+exit /b
